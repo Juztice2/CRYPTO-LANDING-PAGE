396dfd9338ce99952d3617c8fc371b436f1f9493
juztice2 <juzticewilliam@gmail.com>
2026-02-03 07:15:27 +0100

    my first commmit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        display: ["Manrope", "sans-serif"],
+        mono: ["JetBrains Mono", "monospace"],
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        dmsans: ["DM Sans", "sans-serif"],
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        jetmono: ["JetBrains Mono", "monospace"],
+        spacemono: ["Space Mono", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
